7280e4907ec14231d513a65c0ad3809378a50d53
ec0m3x <me@example.com>
2023-07-01 19:52:23 +0200

    added comment games.nix

--- modules/programs/games.nix
+++ modules/programs/games.nix
@@ -21,6 +21,10 @@
       #remotePlay.openFirewall = true;          # Ports for Stream Remote Play
     };
     gamemode.enable = true;                     # Better gaming performance
+                                                # Steam: Right-click game - Properties - Launch options: gamemoderun %command%
+                                                # Lutris: General Preferences - Enable Feral GameMode
+                                                #                             - Global options - Add Environment Variables: LD_PRELOAD=/nix/store/*-gamemode-*-lib/lib/libgamemodeauto.so
+
   };
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
