d9c0b1209e0da64d58faa1c2e94d5e80c0825059
3c0m3x <skoch@sks-concept.de>
2023-07-10 07:43:22 +0200

    update tuxedo

--- modules/laptop/tuxedo.nix
+++ modules/laptop/tuxedo.nix
@@ -17,6 +17,6 @@
     extraRules = '' ACTION=="add|change", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="12d1", ATTR{idProduct}=="15bb", ATTR{bNumConfigurations}=="3", ATTR{bConfigurationValue}!="3" ATTR{bConfigurationValue}="3" '';
   };
   
-  hardware.tuxedo-keyboard.enable = true;
+  #hardware.tuxedo-keyboard.enable = true;
 
 }
