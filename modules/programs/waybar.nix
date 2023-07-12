3b086b6f1b373f6e12f30a00d9b0401f1668c019
3c0m3x <skoch@sks-concept.de>
2023-07-12 06:58:26 +0200

    waybar

--- modules/programs/waybar.nix
+++ modules/programs/waybar.nix
@@ -125,7 +125,7 @@
 
           modules-right =
             if hostName == "desktop" then
-              [ "custom/ds4" "custom/mouse" "custom/kb" "custom/pad" "network" "cpu" "memory" "custom/pad" "pulseaudio" "custom/sink" "custom/pad" "clock" "tray" "custom/pad" ]
+              [ "network" "cpu" "memory" "custom/pad" "pulseaudio" "custom/sink" "custom/pad" "clock" "tray" "custom/pad" ]
             else
               [ "cpu" "memory" "custom/pad" "battery" "custom/pad" "backlight" "custom/pad" "pulseaudio" "custom/pad" "clock" "tray" "custom/pad" ];
 
